453f1feb5b23a175d98a123146a5ba19ab0f0d61
volela <96366089+volela@users.noreply.github.com>
2021-12-26 18:34:16 +0200

    add 01.py

---
+++ 01.py
@@ -0,0 +1,27 @@
+print("hello")
+my_var = 10
+print(my_var)
+my_name = "Shon"
+print(my_name)
+is_true = False
+my_list = ["Shon", "Alelov", 31, True, my_name]
+print(my_list[1])
+my_dict = {"name": "Shon",
+           "lname": "Alelov",
+           "age": 24,
+           "is_married": True}
+print(my_dict["name"])
+print(my_dict.keys())
+my_number = 10
+my_other = 5 * "aviel"
+print(my_other)
+
+
+if my_number == 10:
+    print("my_number")
+
+fname = "shon"
+lname = "Alelov"
+print("hello" + fname + " " + lname)
+print(f"hello {fname} {lname}")
+print("hello %s %s" % (lname, fname))
